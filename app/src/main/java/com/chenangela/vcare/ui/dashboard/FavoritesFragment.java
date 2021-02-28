46944d2a6fadb1ecb1eb8376d9fb8166cc503dae
vaporrrr <ryvapor@gmail.com>
2021-02-28 16:59:19 -0500

    Change Navigation Bar

--- app/src/main/java/com/chenangela/vcare/ui/dashboard/FavoritesFragment.java
+++ app/src/main/java/com/chenangela/vcare/ui/dashboard/FavoritesFragment.java
@@ -4,24 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.chenangela.vcare.R;
 
-public class DashboardFragment extends Fragment {
+public class FavoritesFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private FavoritesViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+                ViewModelProviders.of(this).get(FavoritesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_favorites, container, false);
         //final TextView textView = root.findViewById(R.id.text_dashboard);
         //final TextView favorites_title = root.findViewById(R.id.favorites_title);
--- app/src/main/java/com/chenangela/vcare/ui/dashboard/FavoritesViewModel.java
+++ app/src/main/java/com/chenangela/vcare/ui/dashboard/FavoritesViewModel.java
@@ -4,13 +4,13 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class FavoritesViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public FavoritesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is favorites fragment");
     }
 
     public LiveData<String> getText() {
