dc34c03a16a1d55ad70b31d0b3c657c5cac8e2b6
richardyin99 <richard.yin99@gmail.com>
2021-03-14 20:06:34 -0400

    Calendar and Favorites classes

---
+++ app/src/main/java/com/chenangela/vcare/ui/dashboard/FavoritesFragment.java
@@ -0,0 +1,34 @@
+package com.chenangela.vcare.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.chenangela.vcare.R;
+
+public class FavoritesFragment extends Fragment {
+
+    private FavoritesViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(FavoritesViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_favorites, container, false);
+        //final TextView textView = root.findViewById(R.id.text_dashboard);
+        //final TextView favorites_title = root.findViewById(R.id.favorites_title);
+
+        /*dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                favorites_title.setText(s);
+            }
+        });*/
+        return root;
+    }
+}
